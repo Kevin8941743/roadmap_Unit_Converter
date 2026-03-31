6c7cb976779c9450a4b6a06ffd5ee8ef17243693
Kevin thapa <bobmarley9461238@gmail.com>
2026-03-31 15:48:51 +0100

    take the values from the user

---
+++ weight.js
@@ -0,0 +1,9 @@
+document.querySelector("#submit").addEventListener("click", function() {
+
+
+    let weight = Number(document.getElementById("item_one").value)
+    let newValue = 0
+    let rounded = 0
+    const convertFrom = document.getElementById("item_two").value.toLowerCase()
+    const convertTo = document.getElementById("item_three").value.toLowerCase()
+
