d0b15b0ab9e52c20cf9f2225abe99f99995744c6
Kevin thapa <bobmarley9461238@gmail.com>
2026-03-31 15:49:17 +0100

    logic to handle conversions

--- weight.js
+++ weight.js
@@ -7,3 +7,22 @@ document.querySelector("#submit").addEventListener("click", function() {
     const convertFrom = document.getElementById("item_two").value.toLowerCase()
     const convertTo = document.getElementById("item_three").value.toLowerCase()
 
+    if (convertFrom === "milligram" && convertTo === "gram") {
+        newValue = weight / 1000
+        rounded = newValue.toFixed(3)
+        document.getElementById("data").textContent = `${weight}mg = ${rounded}g`
+
+    } else if (convertFrom === "milligram" && convertTo === "kilogram") {
+        newValue = weight / 1000000
+        rounded = newValue.toFixed(3)
+        document.getElementById("data").textContent = `${weight}mg = ${rounded}kg`
+
+    } else if (convertFrom === "milligram" && convertTo === "ounce") {
+        newValue = weight / 28350
+        rounded = newValue.toFixed(3)
+        document.getElementById("data").textContent = `${weight}mg = ${rounded}oz`
+
+    } else if (convertFrom === "milligram" && convertTo === "pound") {
+        newValue = weight / 453600
+        rounded = newValue.toFixed(3)
+        document.getElementById("data").textContent = `${weight}mg = ${rounded}lb`
