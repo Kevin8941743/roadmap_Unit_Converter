2a91836302a7349f3cd32af349b3215a5b16aa5f
Kevin thapa <bobmarley9461238@gmail.com>
2026-03-31 15:41:00 +0100

    Providing the logic to perform correct temperature calculations

--- temperature.js
+++ temperature.js
@@ -5,3 +5,33 @@ document.querySelector("#submit").addEventListener("click", function() {
     let rounded = 0
     const convertFrom = document.getElementById("item_two").value.toLowerCase()
     const convertTo = document.getElementById("item_three").value.toLowerCase()
+
+    if (convertFrom === "celsius" && convertTo === "fahrenheit") {
+        newValue = (temperature * 9/5) + 32
+        rounded = newValue.toFixed(3)
+        document.getElementById("data").textContent = `${temperature}℃ = ${rounded}°F`
+
+    } else if (convertFrom === "fahrenheit" && convertTo === "celsius") {
+        newValue = (temperature - 32) * 5/9
+        rounded = newValue.toFixed(3)
+        document.getElementById("data").textContent = `${temperature}°F = ${rounded}℃`
+
+    } else if (convertFrom === "celsius" && convertTo === "kelvin") {
+        newValue = temperature + 273.15
+        rounded = newValue.toFixed(3)
+        document.getElementById("data").textContent = `${temperature}℃ = ${rounded}K`
+    
+    } else if (convertFrom === "kelvin" && convertTo === "celsius") {
+        newValue = temperature - 273.15
+        rounded = newValue.toFixed(3)
+        document.getElementById("data").textContent = `${temperature}K = ${rounded}℃`
+
+    } else if (convertFrom === "fahrenheit" && convertTo === "kelvin") {
+        newValue = (temperature - 32) * (5/9) + 273.15
+        rounded = newValue.toFixed(3)
+        document.getElementById("data").textContent = `${temperature}°F = ${rounded}K`
+
+    } else if (convertFrom === "kelvin" && convertTo === "fahrenheit") {
+        newValue = (temperature - 273.15) * 9/5 + 32
+        rounded = newValue.toFixed(3)
+        document.getElementById("data").textContent = `${temperature}K = ${rounded}°F`
